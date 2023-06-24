15ec0fa12abb468ed6c5fd6eb85f97a0cff376fc
arish <arish@sparkosol.com>
2023-06-24 13:19:28 +0500

    basic implementation for auth session.

---
+++ lib/src/auth/auth_session.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+abstract class AuthSessionStorage<T, U> {
+  FutureOr<void> clearUser();
+
+  FutureOr<void> clearToken();
+
+  FutureOr<T> readUser();
+
+  FutureOr<U> readToken();
+
+  FutureOr<void> saveUser(T user);
+
+  FutureOr<void> saveToken(U token);
+}
+
+final class AuthSession<T, U> {
+  AuthSession({required AuthSessionStorage<T, U> storage}) : _storage = storage;
+
+  final AuthSessionStorage<T, U> _storage;
+
+  T? _user;
+  U? _token;
+
+  bool get isSignedIn => _token != null;
+
+  U get token => _token!;
+
+  T get user => _user!;
+
+  FutureOr<void> initialize() async {
+    _user = await _storage.readUser();
+    _token = await _storage.readToken();
+  }
+
+  Future<void> signIn(T user, U token) async {
+    await _storage.saveUser(user);
+    await _storage.saveToken(token);
+  }
+
+  Future<void> signOut() async {
+    await _storage.clearUser();
+    await _storage.clearToken();
+  }
+}
